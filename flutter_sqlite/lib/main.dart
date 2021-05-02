72cd5458d2b9fb623784f0f56f8a59d2c7191d2d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2021-05-02 16:57:28 +0600

    IGNORE:

--- flutter_sqlite/lib/main.dart
+++ flutter_sqlite/lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter SQLite Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'NextG Note',
       home: HomePage(),
     );
   }
